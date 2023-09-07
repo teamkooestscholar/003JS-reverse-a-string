c02e8ed1fd580099d428fac6f8bae1bbbcdfce52
cortezrc6 <82867552+cortezrc6@users.noreply.github.com>
2023-09-08 00:13:43 +0800

    Reverse String Rolly Cortez CpE-401

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,15 @@
-// Function to reverse a string
-function reverseString(_) {
-  // Put your code here
+function reverseString() {
+  
+  var inputElement = document.getElementById("inputString");
+  var inputString = inputElement.value;
+
+  var reversedString = "";
+
+
+  for (var i = inputString.length - 1; i >= 0; i--) {
+      reversedString += inputString.charAt(i);
+  }
+
+  var outputElement = document.getElementById("outputString");
+  outputElement.textContent = reversedString;
 }
