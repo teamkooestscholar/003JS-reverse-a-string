f225407fe9cd80d1a1a52964797bdc77c9a77a8b
lindraaa <chancruz739@gmail.com>
2023-09-08 16:18:43 +0800

    myfunction reverse

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,12 @@
-// Function to reverse a string
-function reverseString(_) {
-  // Put your code here
+function reverseString(inputString){
+  let n_str = "";
+
+  for(let i = inputString.length -1; i>=0; --i){
+      n_str += inputString[i];
+      
+  }
+  return n_str;
 }
+
+console.log(reverseString("hello"));
+console.log(reverseString("my world! @"));
