63289bbdebe1081f8eda77679419546b8952062c
Prgrmmr633 <airesmacalino09@gmail.com>
2023-09-10 07:22:00 +0800

    update

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,30 @@
-// Function to reverse a string
-function reverseString(_) {
-  // Put your code here
+function reverseAndDisplay(method) {
+  const inputString = document.getElementById('inputString').value;
+  let reversed1, reversed2;
+
+  if (method === 'reverseString1') {
+    reversed1 = reverseString1(inputString);
+  } else if (method === 'reverseString2') {
+    reversed2 = reverseString2(inputString);
+  } else {
+    reversed1 = reversed2 = 'Invalid method';
+  }
+
+  document.getElementById('reversedString1').textContent = reversed1;
+  document.getElementById('reversedString2').textContent = reversed2;
+}
+
+function reverseString1(inputString) {
+  const regex = /[\W\s]/g;
+  return inputString.replace(regex, '').split('').reverse().join('');
+}
+
+function reverseString2(inputString) {
+  const regex = /[\W\s]/g;
+  inputString = inputString.replace(regex, '');
+  let reversed = '';
+  for (let i = inputString.length - 1; i >= 0; i--) {
+    reversed += inputString.charAt(i);
+  }
+  return reversed;
 }
