e6b0fce4e2e64a329af3b2553ac65c2aa2271698
KimKris <kimberlykrismanuel@gmail.com>
2023-09-10 22:12:09 +0800

    Takearest:)

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,24 @@
 // Function to reverse a string
-function reverseString(_) {
-  // Put your code here
+function reverseString(inputString) {
+  let n_str = "";
+
+  for (let i = inputString.length - 1; i >= 0; i--) {
+    n_str += inputString[i];
+  }
+  return n_str;
 }
+
+function handleFormSubmit(event) {
+  event.preventDefault(); 
+
+  const inputElement = document.getElementById("inputString");
+  const inputValue = inputElement.value;
+
+  const reversedString = reverseString(inputValue);
+
+  const outputElement = document.getElementById("output");
+  outputElement.textContent = "Reversed String: " + reversedString;
+}
+
+const formElement = document.getElementById("reverseForm");
+formElement.addEventListener("submit", handleFormSubmit);
