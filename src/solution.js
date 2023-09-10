b0d128e1c827b0512899833c959976e27a9d09c4
SparrowNado <123578310+SparrowNado@users.noreply.github.com>
2023-09-10 10:27:12 +0000

    Added Solution and HTML

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,34 @@
-// Function to reverse a string
-function reverseString(_) {
-  // Put your code here
+
+
+
+function StringReverse(str) {
+
+    let reversedString = "";
+
+    for (let i = str.length - 1; i >= 0; i--) {
+            reversedString += str[i];
+
+
+    }
+
+    return reversedString;
+   
+
 }
+
+function ProcessReverse() {
+
+const userInput =  document.getElementById("userInput").value;
+const flippedString = StringReverse(userInput);
+
+
+document.getElementById("str").textContent = flippedString;
+
+}
+
+
+
+
+const button = document.getElementById("checkButton");
+button.addEventListener("click", ProcessReverse);
+
