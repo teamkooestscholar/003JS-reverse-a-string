917e6c9bb3ff521eac2a84acbb5d629a6d0810a7
Minato Arisato <123571129+ZackFair-5698@users.noreply.github.com>
2023-09-10 16:27:31 +0800

    edited ver

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,15 @@
-// Function to reverse a string
-function reverseString(_) {
-  // Put your code here
+function reverseAndDisplay() {
+  const inputElement = document.getElementById('inputString');
+  const resultElement = document.getElementById('result');
+  const inputString = inputElement.value;
+  const reversedString = reverseString(inputString);
+  resultElement.textContent = `Reversed String: ${reversedString}`;
+}
+
+function reverseString(inputString) {
+  let reversedString = '';
+  for (let i = inputString.length - 1; i >= 0; i--) {
+    reversedString += inputString[i];
+  }
+  return reversedString;
 }
