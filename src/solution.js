05b402d765482e66e3002e7ddb943069152f5289
ChristianPaulo24 <123344269+ChristianPaulo24@users.noreply.github.com>
2023-09-10 22:49:35 +0800

    solution.js

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,22 @@
-// Function to reverse a string
-function reverseString(_) {
-  // Put your code here
-}
+   // Define the reverseString function
+   function reverseString(inputString) {
+    let reversedString = '';
+    for (let i = inputString.length - 1; i >= 0; i--) {
+      reversedString += inputString[i];
+    }
+    return reversedString;
+  }
+
+  // Function to reverse the input string and display the result
+  function reverseAndDisplay() {
+    // Get the user's input
+    const inputElement = document.getElementById('inputString');
+    const inputString = inputElement.value;
+
+    // Call the reverseString function
+    const reversedResult = reverseString(inputString);
+
+    // Display the reversed result
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = reversedResult;
+  }
