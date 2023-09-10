f75e3a3f9bf2f7a8a7f8419e343a0199c65d62ee
ysabelandreii <123624285+ysabelandreii@users.noreply.github.com>
2023-09-10 15:27:27 +0800

    Update solution.js
    Submission of solution.js for 003 - js - Reverse a string

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,39 @@
-// Function to reverse a string
-function reverseString(_) {
-  // Put your code here
-}
+function reverseString(inputString) {
+    var reversed = '';
+    var word = '';
+    var isWord = false;
+  
+    for (var i = 0; i < inputString.length; i++) {
+      var char = inputString.charAt(i);
+  
+      if (isWordCharacter(char)) {
+        word = char + word;
+        isWord = true;
+      } else {
+        if (isWord) {
+          reversed += word;
+          word = '';
+          isWord = false;
+        }
+        reversed += char;
+      }
+    }
+  
+    if (isWord) {
+      reversed += word;
+    }
+  
+    return reversed;
+  }
+  
+  function isWordCharacter(char) {
+    return /^[a-zA-Z0-9]+$/.test(char);
+  }
+  
+  function reverseAndDisplay() {
+    var inputElement = document.getElementById('inputString');
+    var outputElement = document.getElementById('output');
+    var inputString = inputElement.value;
+    var reversedString = reverseString(inputString);
+    outputElement.textContent = reversedString;
+  }
