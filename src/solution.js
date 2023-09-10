bcf418004fc39cdbe221193bf27c5889674e61de
Ecniov <123645284+Ecniov@users.noreply.github.com>
2023-09-10 22:31:30 +0800

    vincedeato

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,38 @@
-// Function to reverse a string
-function reverseString(_) {
-  // Put your code here
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function reverseUserInput() {
+  rl.question("Enter a string: ", (userInput) => {
+    rl.close();
+
+    // Check if the user entered an empty string
+    if (userInput.trim() === "") {
+      console.log("No input provided");
+    } else {
+      // Reverse the user input using the modified reverseString function
+      const reversed = reverseString(userInput);
+
+      // Output the reversed string
+      console.log(reversed);
+    }
+  });
 }
+
+// Define the modified reverseString function
+function reverseString(inputString) {
+  let reversed = '';
+
+  // Iterate through the inputString from end to start
+  for (let i = inputString.length - 1; i >= 0; i--) {
+    reversed += inputString[i];
+  }
+
+  return reversed;
+}
+
+// Call the reverseUserInput function to get user input and reverse it
+reverseUserInput();
