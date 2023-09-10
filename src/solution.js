be6549dd182db6aefd9a50ceca0cb19f07e68dad
shinteo <124492024+ShinPio@users.noreply.github.com>
2023-09-10 16:32:58 +0800

    JS Reverse a String

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,17 @@
-// Function to reverse a string
-function reverseString(_) {
-  // Put your code here
+// Bonus Challenge 1: Handle special characters or whitespace gracefully using regular expressions
+function reverseString(inputString) {
+  // Use a regular expression to match any character, including special characters and whitespace
+  const regex = /./g;
+
+  // Replace each character with its reverse
+  return inputString.replace(regex, (match) => match + inputString).slice(0, -inputString.length);
+}
+
+// Bonus Challenge 2: Write a version of the function without using split, reverse, or join methods
+function reverseStringWithoutSplit(inputString) {
+  let reversed = '';
+  for (let i = inputString.length - 1; i >= 0; i--) {
+    reversed += inputString[i];
+  }
+  return reversed;
 }
