43039cfd1fc90ffe7b4ffa325683a5f8398747ac
JouleMei <121792629+JouleMei@users.noreply.github.com>
2023-09-12 23:38:42 +0800

    Added solution

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,16 @@
 // Function to reverse a string
-function reverseString(_) {
-  // Put your code here
+function reverseAndDisplay() {
+  const value = document.getElementById('value');
+  const result = document.getElementById('result');
+  const value = value.value;
+  const reversedString = reverseString(value);
+  resultElement.textContent = `Reversed String: ${reversedString}`;
+}
+
+function reverseString(value) {
+  let reversedString = '';
+  for (let i = value.length - 1; i >= 0; i--) {
+    reversedString += value[i];
+  }
+  return reversedString;
 }
