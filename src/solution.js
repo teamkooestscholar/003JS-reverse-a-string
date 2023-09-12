572b951e719a869938528a9b2496cbd7bf297c7d
RaphaelSyrrusDNucum <144664548+RaphNucum@users.noreply.github.com>
2023-09-12 23:47:51 +0800

    raph

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,32 @@
-// Function to reverse a string
-function reverseString(_) {
-  // Put your code here
+public class ReverseString {
+    public static String reverse(String input) {
+        if (input == null) {
+            return null; // Handle null input if needed
+        }
+
+        char[] charArray = input.toCharArray();
+        int start = 0;
+        int end = charArray.length - 1;
+
+        while (start < end) {
+            // Swap characters at start and end positions
+            char temp = charArray[start];
+            charArray[start] = charArray[end];
+            charArray[end] = temp;
+
+            // Move the pointers towards each other
+            start++;
+            end--;
+        }
+
+        return new String(charArray);
+    }
+
+    public static void main(String[] args) {
+        String original = "Hello, World!";
+        String reversed = reverse(original);
+
+        System.out.println("Original: " + original);
+        System.out.println("Reversed: " + reversed);
+    }
 }
