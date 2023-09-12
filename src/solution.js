e8fb21732aa3f92e7da6f52af218ca213568efec
Cy12swe <104562626+Cy12swe@users.noreply.github.com>
2023-09-12 21:55:46 +0800

    Function to reverse a string and takes special characters gracefully. Another version without the split, join, and reverse method.

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,53 @@
-// Function to reverse a string
-function reverseString(_) {
-  // Put your code here
+ //function to reverse a string
+ function reverseString(str) {
+  let arr = str.split("");
+  
+  let reversedArr = [];
+  
+  for(let i = arr.length-1; i >= 0; i--) {
+    if(/[\s\W]/.test(arr[i])) {
+      reversedArr[i] = arr[i];
+    } else {
+      let j = 0;
+      while(reversedArr[i+j] !== undefined){
+        j++;
+      }
+      reversedArr[i + j] = arr[i];
+    }
+  }
+  
+  return reversedArr.join("");
+}
+
+
+
+
+
+  //function without using split join and reverse method
+  function reverseStringWithoutSplitJoin(inputString) {
+  let reversedString = '';
+  
+  function isAlphanumeric(char) {
+    return /[a-zA-Z0-9]/.test(char);
+  }
+  
+  let left = 0;
+  let right = inputString.length - 1;
+  
+  while (left <= right) {
+    if (isAlphanumeric(inputString[left]) && isAlphanumeric(inputString[right])) {
+      reversedString += inputString[right];
+      right--;
+      left++;
+    } else {
+      if (!isAlphanumeric(inputString[left])) {
+        left++;
+      }
+      if (!isAlphanumeric(inputString[right])) {
+        right--;
+      }
+    }
+  }
+  
+  return reversedString;
 }
