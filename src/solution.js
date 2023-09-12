40628a75e0887aed16cb97a913c3ce9f74e73696
kellydeguzman <kellydeguzman15@gmail.com>
2023-09-12 21:34:02 +0800

    created

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,13 @@
-// Function to reverse a string
-function reverseString(_) {
-  // Put your code here
+// 003 Function to reverse a string
+function reverseString(inputString){
+  let i_string = "";
+
+  for(let i = inputString.length -1; i>=0; --i){
+      i_string += inputString[i];
+      
+  }
+  return i_string;
+
 }
+console.log(reverseString("Hello"));
+console.log(reverseString("World!!! @"));
