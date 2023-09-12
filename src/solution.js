5e679788ccaa26519bacc46d92ce5d15eed142a8
tobywan1 <shantoby00@yahoo.com>
2023-09-12 22:12:22 +0800

    Update solution.js

--- src/solution.js
+++ src/solution.js
@@ -2,4 +2,15 @@
 function reverseString(_) {
   // Put your code here
   def
+}  
+  const charArray = inputString.split('');
+  const reversedArray = charArray.reverse();
+  const reversedString = reversedArray.join('');
+
+  return reversedString;
 }
+
+
+const originalString = "Hello, World!";
+const reversed = reverseString(originalString);
+console.log(reversed); 
