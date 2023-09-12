7a6d9ffe81b1a0167abdb42386182ba148777656
Tsukkiz <123831791+Tsukkiz@users.noreply.github.com>
2023-09-12 22:20:46 +0800

    JS3Macarayo

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,14 @@
-// Function to reverse a string
-function reverseString(_) {
-  // Put your code here
+function reverseString(inputString) {
+  let reversedString = '';
+
+  for (let i = inputString.length - 1; i >= 0; i--) {
+    reversedString += inputString.charAt(i);
+  }
+
+  return reversedString;
 }
+
+const input = "1 2 3 4 5 6 7 8 9 0";
+const reversed = reverseString(input);
+console.log("The input String is:", input)
+console.log("The Reversed String is:", reversed);
